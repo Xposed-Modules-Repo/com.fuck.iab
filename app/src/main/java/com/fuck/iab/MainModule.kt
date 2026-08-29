5d105aba86091453800d831eb9b686edbc4c885b
staleroot <311373933+staleroot@users.noreply.github.com>
2026-08-29 22:29:39 +0000

    version 1.2.5

--- app/src/main/java/com/fuck/iab/MainModule.kt
+++ app/src/main/java/com/fuck/iab/MainModule.kt
@@ -1,13 +1,14 @@
 package com.fuck.iab
 
 import android.app.Application
-import android.widget.Toast
 import dd.c
-import io.github.libxposed.api.XposedModuleInterface
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 import org.luckypray.dexkit.DexKitBridge
 
 class MainModule : c() {
-    override fun onInitialized(app: Application, param: XposedModuleInterface.PackageLoadedParam, bridge: DexKitBridge) {
-        Toast.makeText(app, "F*IAB Loaded!", Toast.LENGTH_SHORT).show()
+
+    // This is called before Application.onCreate()
+    override fun onInitialized(app: Application, param: PackageLoadedParam, bridge: DexKitBridge) {
+
     }
 }
