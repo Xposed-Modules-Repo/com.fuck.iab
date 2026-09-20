a6687157da46bbe047303468af348ad001286007
staleroot <311373933+staleroot@users.noreply.github.com>
2026-09-20 10:44:47 +0000

    version 1.4.9

--- scripts/src/com.kiloo.subwaysurf.js
+++ scripts/src/com.kiloo.subwaysurf.js
@@ -11,36 +11,25 @@ const observer = Process.attachModuleObserver({
 });
 
 function Hook() {
-    console.log("Hooking GetCurrency method...");
     Il2Cpp.perform(() => {
-        Il2Cpp.domain.assemblies.forEach(a => {
-            a.image.classes.forEach(c => {
-                c.methods.forEach(m => {
-                    if(m.name.includes("GetCurrency") && m.returnType.name === "System.Int32") {
-                        m.implementation = function(arg) {
-                            return 999999;
-                        };
+        const AssemblyCSharp = Il2Cpp.domain.assembly("Assembly-CSharp").image
+        const IAPValidateResponse = AssemblyCSharp.class("SYBO.Subway.Services.IAPValidateResponse");
+        const GetResult = Il2Cpp.corlib.class("System.Runtime.CompilerServices.TaskAwaiter`1").inflate(IAPValidateResponse).method("GetResult");
+        const PurchaseStatus = AssemblyCSharp.class("SYBO.Subway.Meta.PurchaseEventData/PurchaseStatus")
+        const PurchaseStatusSuccess = PurchaseStatus.field("Success").value
+
+        GetResult.implementation = function () {
+            try {
+                const result = GetResult.invokeRaw(this)
+                if(result && !result.isNull()) {
+                    if(result.class.name == "PurchaseEventData") {
+                        result.field("<Status>k__BackingField").value = PurchaseStatusSuccess
                     }
-                });
-            });
-        });
-
-        // const AssemblyCSharp = Il2Cpp.domain.assembly("Assembly-CSharp").image
-        // const IAPValidateResponse = AssemblyCSharp.class("SYBO.Subway.Services.IAPValidateResponse");
-        // const GetResult = Il2Cpp.corlib.class("System.Runtime.CompilerServices.TaskAwaiter`1").inflate(IAPValidateResponse).method("GetResult");
-        // const None = AssemblyCSharp.class("SYBO.Subway.Meta.PurchaseError").field("None").value
-
-        // GetResult.implementation = function () {
-        //     try {
-        //         const result = GetResult.invokeRaw(this)
-        //         if(result && !result.isNull() && result.class.name == "IAPValidateResponse") {
-        //             result.field("PurchaseError").value = None
-        //             result.field("ProductId").value = Il2Cpp.string("subwaysurfers.coins.01")
-        //         }
-        //         return result
-        //     } catch(e) {
-        //         return ptr(0)
-        //     }
-        // };
+                }
+                return result
+            } catch(e) {
+                return ptr(0)
+            }
+        };
     });
 }
